bb5017339cb4c9b42fed9025d76ec3a210a48681
João Neto <joaoneto7499@gmail.com>
2022-10-08 01:07:12 -0300

    finish style of transactions cards

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,9 +1,57 @@
 import React from "react";
-import {Container} from "./styles";
+import {
+    Container,
+    Title,
+    Amout,
+    Footer,
+    Category,
+    Icon,
+    CategoryName,
+    Date
+} from "./styles";
 
-export function TransactionCard() {
+interface Category {
+    name: string //Salario Mensal
+    icon: string
+}
+
+export interface TransactionCardProps {
+    type: 'positive' | 'negative'
+    title: string,
+    amount: string,
+    category: Category,
+    date: string,
+
+}
+
+interface Props {
+    data: TransactionCardProps
+}
+
+
+export function TransactionCard({data}: Props) {
     return (
         <Container>
+            <Title>
+                {data.title}
+            </Title>
+            <Amout type={data.type}>
+                {data.type === 'negative' && '- '}
+                {data.amount}
+            </Amout>
+
+            <Footer>
+                <Category>
+                    <Icon name={data.category.icon}/>
+                    <CategoryName>
+                        {data.category.name}
+                    </CategoryName>
+                </Category>
+
+                <Date>
+                    {data.date}
+                </Date>
+            </Footer>
         </Container>
     );
 }
